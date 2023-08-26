21a3461b2a3169a46cdc1faeabbcd1055af516c6
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-08-26 12:03:58 +0300

    finished creating functions using  react query to fetch  data from json api

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,7 +18,7 @@ const Post = ({id}:PostProps) => {
         enabled:postQuery?.data?.userId !=null,
         queryFn:() => getUser(postQuery.data.userId)
     })
-    console.log(postQuery)
+    
 
     if(postQuery.isLoading) return <h1>Loading ...</h1>
 
